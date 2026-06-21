d53cb73fb3ae53dcb0bcc3ec24b25438c14079f5
Digisema <senrima.ms@gmail.com>
2026-06-21 13:53:13 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: './', // Ubah bagian ini menjadi titik dan garis miring
   plugins: [react()],
   build: {
     rollupOptions: {
